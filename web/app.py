4bcae962a23557438c6081a65b2e596e8fdafc35
ukaratepe <ukaratepe@etu.edu.tr>
2026-01-20 14:13:54 +0300

    PostreSQL entegrasyonu tamamlandı.

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import pika
 import json
-import sqlite3
 import threading
 from datetime import datetime
 import os
@@ -13,30 +13,27 @@ RABBIT_HOST = os.getenv("RABBIT_HOST", "rabbitmq")
 QUEUE_NAME = os.getenv("QUEUE_NAME", "interpol_queue")
 DB_NAME=os.getenv("DB_NAME", "interpol.db")
 
-# --- VERİTABANI AYARLARI ---
-def get_db_connection():
-    conn = sqlite3.connect('DB_NAME', check_same_thread=False)
-    conn.row_factory = sqlite3.Row
-    return conn
+# postgresql://KULLANICI:SIFRE@SERVIS_ADI:PORT/VERITABANI_ADI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://interpol_user:gizlisifre123@db:5432/interpol_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+#Veritabanı objesini oluştur.
+db = SQLAlchemy(app)
 
-#Veritabanına bağlan ve criminals tablosunu oluştur
-def init_db():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    # Tabloyu oluştur (Eğer yoksa)
-    cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS criminals
-                   (
-                       entity_id TEXT PRIMARY KEY,
-                       name TEXT,
-                       nationalities TEXT,
-                       timestamp TEXT,
-                       status TEXT DEFAULT 'NEW'
-                   )
-    ''')
-    conn.commit()
-    conn.close()
+#Artık sorgu yazmıyoruz Class tanımlıyoruz.
+class Criminal(db.Model):
+    __tablename__ = 'criminals'
+
+    id = db.Column(db.Integer, primary_key=True)
+    entity_id = db.Column(db.String(50), unique=True)  # ID tekrar etmesin
+    name = db.Column(db.String(200))
+    nationalities = db.Column(db.String(200))
+    timestamp = db.Column(db.String(50))
+    alarm = db.Column(db.Boolean, default=False)
+    status = db.Column(db.String(50), default="NEW")
+
+    def __repr__(self):
+        return f'<Criminal {self.name}>'
 
 def convert_to_country(code_string):
     if not code_string:
@@ -47,7 +44,7 @@ def convert_to_country(code_string):
     nationalities = []
 
     for code in codes:
-        code = code.strip()
+        code = code.strip() #Boşlukları ortadan kaldır.
         try:
             nationality = pycountry.countries.get(alpha_2=code)
             if nationality:
@@ -59,9 +56,9 @@ def convert_to_country(code_string):
 
     return ', '.join(nationalities)
 
-# --- ARKA PLAN GÖREVİ: KUYRUK DİNLEYİCİSİ ---
+
+# --- DÜZELTİLMİŞ HALİ ---
 def consume_queue():
-    # Docker içinde olduğumuz için host='rabbitmq' olmalı!
     try:
         connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST))
         channel = connection.channel()
@@ -70,49 +67,55 @@ def consume_queue():
         def callback(ch, method, properties, body):
             data = json.loads(body)
             notices = data.get('_embedded', {}).get('notices', [])
+            print(f"📥 [Web Container] Kuyruktan {len(notices)} veri geldi...")
 
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            print(f"📥 [Web Container] Kuyruktan {len(notices)} veri geldi, kaydediliyor...")
+            # 1. DÜZELTME: Context döngünün DIŞINA alındı.
+            with app.app_context():
+                degisiklik_sayisi = 0
 
-            degisiklik_sayisi = 0
-            for person in notices:
-                try:
-                    entity_id = person.get('entity_id')
-                    name = f"{person.get('forename', '')} {person.get('name', '')}"
-                    nationalities = str(person.get('nationalities', []))
-                    nationalities = convert_to_country(nationalities)
-                    # Burada basitlik olsun diye timestamp'i şu anki zaman alıyoruz
-                    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                for person in notices:
+                    try:
+                        entity_id = person.get('entity_id')
+                        name = f"{person.get('forename', '')} {person.get('name', '')}".strip()
+                        raw_nationalities = str(person.get('nationalities', []))
+                        nationalities = convert_to_country(raw_nationalities)
+                        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                    #Kullanıcı zaten mevcut mu ? kontrol et
-                    existing_user = conn.execute('SELECT * FROM criminals WHERE entity_id = ?', (entity_id,)).fetchone()
-                    if existing_user:
-                       if existing_user['nationalities'] != nationalities or existing_user['name'] != name:
-                          #Önceden kayıt edilmiş bilgi değişti
-                          cursor.execute(''' UPDATE criminals
-                                          SET nationalities = ?, name = ?, timestamp = ?, status = 'UPDATED' 
-                                          WHERE entity_id = ? ''', (nationalities, name, now, entity_id))
-                          degisiklik_sayisi += 1
-                          print(f"GÜNCELLEME! {name} bilgilerinde değişiklik tespit edildi.")
-                       else:
-                          pass
-                    else:
-                        cursor.execute('''
-                            INSERT OR REPLACE INTO criminals (entity_id, name, nationalities, timestamp, status)
-                            VALUES (?, ?, ?, ?, 'NEW')
-                        ''', (entity_id, name, nationalities, now))
-                        degisiklik_sayisi += 1
-                except Exception as e:
-                    print(f"Hata: {e}")
+                        existing_user = Criminal.query.filter_by(entity_id=entity_id).first()
 
-            print(f"{degisiklik_sayisi} kayıtta işlem yapıldı.")
-            conn.commit()
-            conn.close()
+                        if existing_user:
+                            # Değişiklik kontrolü
+                            if existing_user.nationalities != nationalities or existing_user.name != name:
+                                existing_user.nationalities = nationalities
+                                existing_user.name = name
+                                existing_user.timestamp = now
+                                existing_user.status = "UPDATED"
+                                degisiklik_sayisi += 1
+                                print(f"GÜNCELLEME: {name}")
+                        else:
+                            yeni_suclu = Criminal(
+                                entity_id=entity_id,
+                                name=name,
+                                nationalities=nationalities,
+                                timestamp=now,
+                                alarm=False,
+                                status="NEW"
+                            )
+                            db.session.add(yeni_suclu)
+                            degisiklik_sayisi += 1
+
+                    except Exception as e:
+                        print(f"Veri işleme hatası: {e}")
+
+                # 2. DÜZELTME: Döngü bittikten sonra TEK SEFERDE kaydet (Toplu işlem)
+                if degisiklik_sayisi > 0:
+                    db.session.commit()
+                    print(f"💾 {degisiklik_sayisi} değişiklik kaydedildi.")
 
         channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
         print(' [*] Kuyruk dinleme servisi başladı...')
         channel.start_consuming()
+
     except Exception as e:
         print(f"RabbitMQ Bağlantı Hatası: {e}")
 
@@ -120,30 +123,29 @@ def consume_queue():
 # --- WEB SİTESİ ROTALARI ---
 @app.route('/')
 def index():
-    conn = get_db_connection()
     # Veritabanındaki herkesi çek
-    criminals = conn.execute('SELECT * FROM criminals ORDER BY timestamp DESC').fetchall()
-    conn.close()
+    criminals = Criminal.query.order_by(Criminal.id.asc()).all()
 
     #Jinja'ya göndermeden önce veriyi işle
     criminal_list = []
     now = datetime.now()
 
-    for row in criminals:
-        criminal = dict(row)
-        timestamp = datetime.strptime(criminal['timestamp'], '%Y-%m-%d %H:%M:%S')
+    for criminal in criminals:
+        timestamp = datetime.strptime(criminal.timestamp, '%Y-%m-%d %H:%M:%S')
         difference_on_seconds = (now - timestamp).total_seconds()
-        is_updated = criminal['status'] == 'UPDATED'
+        is_updated = criminal.status == 'UPDATED'
         if is_updated and difference_on_seconds < 30: #60 saniye içinde güncellendiyse alarm çalsın.
-            criminal['alarm'] = True
+            criminal.alarm = True
         else:
-            criminal['alarm'] = False
+            criminal.alarm = False
         criminal_list.append(criminal)
     return render_template('index_with_bootstrap.html', criminals=criminal_list)
 
 
 if __name__ == '__main__':
-    init_db()
+
+    with app.app_context():
+        db.create_all()
 
     # Kuyruk dinleyicisini ayrı bir "Thread" (iş parçacığı) olarak başlat
     # Bu sayede Flask sunucusu çalışırken arka planda veri kaydı devam etsin.
